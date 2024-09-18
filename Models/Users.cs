881931c50871f5440af078e74f3171324e39332d
lab_services_student <ST10056728@vcconnect.edu.za>
2024-09-18 13:07:38 +0200

    getting new roles

---
+++ Models/Users.cs
@@ -0,0 +1,12 @@
+﻿namespace Apptivate_UQMS_WebApp.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+        public DateTime LastSeen { get; set; }
+    }
+
+}
