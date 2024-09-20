301defa741df5dae97bc7a7a2a044e518b0fbfa1
trent <trentfisher017@gmail.com>
2024-09-20 16:54:56 +0200

    merge

--- obj/Debug/net8.0/Apptivate_UQMS_WebApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/Apptivate_UQMS_WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Apptivate_UQMS_WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd2eb32b9fde38c4b4f49e6f65e57f02ca3de15a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Apptivate_UQMS_WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Apptivate_UQMS_WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
