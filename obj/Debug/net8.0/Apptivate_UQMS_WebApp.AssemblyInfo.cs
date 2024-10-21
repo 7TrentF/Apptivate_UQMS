ac29a9d22d8749122c8adb9f37c08bf6a8036e99
trent <trentfisher017@gmail.com>
2024-10-21 18:57:27 +0200

    added styling to ResolvedTicket details view

--- obj/Debug/net8.0/Apptivate_UQMS_WebApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/Apptivate_UQMS_WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Apptivate_UQMS_WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a15cee2684f5e692d4356f530d17e2ed635dec7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f108d2702a9b7cafb82e7f84fcfb6eef94342d45")]
 [assembly: System.Reflection.AssemblyProductAttribute("Apptivate_UQMS_WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Apptivate_UQMS_WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
