bb10bbcbdb033e5532e7ce9874a45a59ab9b1623
trent <trentfisher017@gmail.com>
2024-11-25 14:19:46 +0200

    unit test

--- obj/Debug/net8.0/Apptivate_UQMS_WebApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/Apptivate_UQMS_WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Apptivate_UQMS_WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0593a48740be1b62a2889001063135bdc1484c9d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Apptivate_UQMS_WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Apptivate_UQMS_WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
