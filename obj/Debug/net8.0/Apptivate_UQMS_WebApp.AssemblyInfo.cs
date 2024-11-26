4d874bb0f59bd614941ce1e47fdd91779ab61df0
trent <trentfisher017@gmail.com>
2024-11-26 18:00:15 +0200

    Fixed Sso

--- obj/Debug/net8.0/Apptivate_UQMS_WebApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/Apptivate_UQMS_WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Apptivate_UQMS_WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc2f1b1edf5fbfc6d5e4d7f4b2b920da9c43354d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Apptivate_UQMS_WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Apptivate_UQMS_WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
